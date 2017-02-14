625cb38bc51fa205258ef8aeff8b25b79057b7e3
John Linneman and Oed Ronne <john+oed@>
2017-02-13 19:19:44 -0800

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+  $('#blanks form').submit(function(event) {
+    var person1Input = $("input#person1").val();
+    $(".person1").text(person1Input);
+  $("#letter").show();
+  event.preventDefault();
+  });
+});
+
   // $("#blanks form").submit(function(event){
   //   var person1Input = $("input#person1").val();
   //   var person2Input = $("input#person2").val();
@@ -19,4 +27,3 @@ $(document).ready(function(){
   //
   //   event.preventDefault();
   // });
-});
